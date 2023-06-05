1a8d874f3ad0f8c1267b1952fc167d6c1d59d767
Dmitriy <Dmitriy.Kh.78@yandex.ru>
2023-06-05 21:46:43 +0300

    Z1

--- DZ_2/Program.cs
+++ DZ_2/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more  1111 444
-Console.WriteLine("Hello, World!");
+﻿//Программа которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.Write("Введите трехзначное число ");
+int Number = Convert.ToInt32(Console.ReadLine());
+int NumberA = Number / 10;
+int NumberTwo = NumberA % 10;
+Console.WriteLine( "" + Number + "->" + NumberTwo);
