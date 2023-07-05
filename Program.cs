db0a13046075bf6225ac063a6bcd88b7ccb9b16e
Dmitriy <Dmitriy.Kh.78@yandex.ru>
2023-07-05 12:13:24 +0300

    FirstArray

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// Написать программу, которая из имеющегося массива строк формирует новый массив из строк, 
+//длина которых меньше, либо равна 3 символам.
+Console.Write("Введите колличество элементов массива ");
+int NumberElementsArray = Convert.ToInt32(Console.ReadLine());
+
+ //Console.WriteLine( NumberMvsivElements);
+
+string[] arrayFirst = new string[NumberElementsArray];
+for (int i =0; i < NumberElementsArray; i++)
+{
+    Console.WriteLine($"Введите {i+1} элемент");
+    string NumberElements = Convert.ToString(Console.ReadLine());
+    arrayFirst[i] = NumberElements;
+}
+ 
+
+for (int i =0; i < NumberElementsArray; i++)
+{
+
+    Console.WriteLine();
+    Console.WriteLine(arrayFirst[i]);
+}
