be693d9179862ad3dc270938c772216c3b1de32c
Dmitriy <Dmitriy.Kh.78@yandex.ru>
2023-07-06 13:54:28 +0300

    Otpravka_2

--- Program.cs
+++ Program.cs
@@ -1,52 +1,55 @@
 ﻿// Написать программу, которая из имеющегося массива строк формирует новый массив из строк, 
 //длина которых меньше, либо равна 3 символам.
-Console.Write("Введите колличество элементов массива, но не меньше трех ");
+Console.Write("Введите колличество элементов массива ");
 int NumberElementsArray = Convert.ToInt32(Console.ReadLine());
 int MinLong  = 3;
 
-if (NumberElementsArray >= MinLong)
-{
 
-  string[] arrayFirst = new string[NumberElementsArray];
+  string[] ArrayFirst = new string[NumberElementsArray];
   for (int i =0; i < NumberElementsArray; i++)
   {
-    
     Console.WriteLine($"Введите {i+1} элемент");
     string NumberElements = Convert.ToString(Console.ReadLine())!;
-    arrayFirst[i] = NumberElements;
-  }
+    ArrayFirst[i] = NumberElements;
+  } 
 
-
-  int MinFinalArray = 0;
-  int MaxFinalArray = 3;
-
-  int LongFinalArray = new Random().Next(MinFinalArray,MaxFinalArray);
+  
     
-  string[] FinalArray = new string[LongFinalArray];
-  for (int j = 0; j < LongFinalArray; j++)
+  string[] TwolArray  = new string[ArrayFirst.Length];
+  int count = 0;
+  for (int j = 0; j < NumberElementsArray; j++)
     {
-        int k = new Random().Next(0,NumberElementsArray);
-         FinalArray[j] =  arrayFirst[k];
-    }
+      if (ArrayFirst[j].Length <= MinLong) 
+       {
+         TwolArray[count] =  ArrayFirst[j];
+         count++;
+        }
+     }
 
+  string[] FinalArray  = new string[count];
+  int LongFinalArray =0;
+  for (int j = 0; j < NumberElementsArray; j++)
+    {
+      if (ArrayFirst[j].Length <= MinLong) 
+       {
+         FinalArray[LongFinalArray] =  ArrayFirst[j];
+         LongFinalArray++;
+        }
+     }
 
+    
   void PrintArray(string[] array)
    {
     Console.Write("[");
     for (int i = 0; i < array.Length; i++)
       {
-       Console.Write($" {array[i]} {";"} ");
+       Console.Write($" {"'"} {array[i]} {"'"} {";"} ");
        }
      Console.Write("]");
     }
     
 
-  PrintArray(arrayFirst);
+  PrintArray(ArrayFirst);
   Console.Write("->");
   PrintArray(FinalArray);
 
-}
-else
-{
-    Console.Write("Ошибка! Повторите ввод");
-}
